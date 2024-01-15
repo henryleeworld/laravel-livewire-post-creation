2d25b594dec353986cad2f074a88fe730256dd0d
Henry Lee <henryleeworld@gmail.com>
2024-01-16 00:09:54 +0800

    feat:初始化

---
+++ README.md
@@ -0,0 +1,39 @@
+# Laravel 10 Livewire 文章建立
+
+在撰寫文章之前需要先釐清產出目的，再針對這個目標去撰寫相關的內容，才不會花了許多時間與心力，卻沒有帶來任何的效益，或是產出對達成目標沒有幫助的文章。
+
+## 使用方式
+- 把整個專案複製一份到你的電腦裡，這裡指的「內容」不是只有檔案，而是指所有整個專案的歷史紀錄、分支、標籤等內容都會複製一份下來。
+```sh
+$ git clone
+```
+- 將 __.env.example__ 檔案重新命名成 __.env__，如果應用程式金鑰沒有被設定的話，你的使用者 sessions 和其他加密的資料都是不安全的！
+- 當你的專案中已經有 composer.lock，可以直接執行指令以讓 Composer 安裝 composer.lock 中指定的套件及版本。
+```sh
+$ composer install
+```
+- 產生 Laravel 要使用的一組 32 字元長度的隨機字串 APP_KEY 並存在 .env 內。
+```sh
+$ php artisan key:generate
+```
+- 執行 __Artisan__ 指令的 __migrate__ 來執行所有未完成的遷移。
+```sh
+$ php artisan migrate
+```
+- 執行安裝 Vite 和 Laravel 擴充套件引用的依賴項目。
+```sh
+$ npm install
+```
+- 執行正式環境版本化資源管道並編譯。
+```sh
+$ npm run build
+```
+- 在瀏覽器中輸入已定義的路由 URL 來訪問，例如：http://127.0.0.1:8000。
+- 你可以經由 `/register` 來進行註冊。
+- 完成註冊後，可以經由 `/login` 來進行登入。
+
+----
+
+## 畫面截圖
+![](https://i.imgur.com/a8lLEw2.gif)
+> 首先定一個好的文章標題，其次要試想怎麼樣可以更加輕鬆地理解想要表達的內容
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You are logged in!") }}
+                    <br/>
+                    <br/>
+                    <a href="{{ route('posts.create') }}" type="button" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">{{ __("Create Post") }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
